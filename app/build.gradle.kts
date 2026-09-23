8e083615225e16365fb5f51d1b3fb26bbccca6f9
elxvro <elxvro@gmail.com>
2026-09-23 17:19:10 +0300

    Bump WARSTATE to v0.4.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.elxvro.warstate"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "0.3.1"
+        versionCode = 5
+        versionName = "0.4.0"
     }
 
     buildTypes {
